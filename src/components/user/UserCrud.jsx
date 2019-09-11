071d51e3074db258ae25058ddebac84316e53288
Janer Bastos de Melo <janer.melo@ufopa.edu.br>
2019-09-11 10:36:00 -0300

    Implementando o form do componente UserCrud

--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -10,7 +10,7 @@ const headerProps = {
 
 const baseUrl = "http://localhost:3001/users"
 const initialState = {
-    user: {name: '', emali:''},
+    user: {name: '', email:''},
     list: []
 }
 
@@ -32,7 +32,7 @@ export default class UserCrud extends Component {
         const url = user.id ? `${baseUrl}/${user.id}`: baseUrl
         axios[method](url, user)
             .then(resp => {
-                const list = getUpdateList(resp.data)
+                const list = this.getUpdateList(resp.data)
                 this.setState({ user: initialState.user, list })
             })
     }
@@ -46,10 +46,59 @@ export default class UserCrud extends Component {
         return list
     }
 
+    updateField(event){
+        const user = { ...this.state.user }
+        user[event.target.name] = event.target.value
+        this.setState({ user })
+    }
+
+    renderForm(){
+        return(
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input type="text" className="form-control"
+                                name="name"
+                                value={ this.state.user.name }
+                                onChange={ e => this.updateField(e) }
+                                placeholder="Digite o nome..." />
+                        </div>
+                    </div>
+                    
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>E-Mail</label>
+                            <input type="text" className="form-control"
+                                name="email"
+                                value={ this.state.user.email }
+                                onChange={ e => this.updateField(e) }
+                                placeholder="Digite o e-mail..." />
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+                <div className="row">
+                    <div className="col-12 d-flex justity-content-end">
+                        <button className="btn btn-primary"
+                            onClick={ e => this.save(e) } >
+                            Salvar
+                        </button>
+                        <button className="btn btn-secondary ml-2"
+                            onClick={ e => this.clear(e) } >
+                            Cancelar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     render(){
         return (
             <Main { ...headerProps }>
-                Cadastro de usuários
+                { this.renderForm() }
             </Main>
         )
     }
